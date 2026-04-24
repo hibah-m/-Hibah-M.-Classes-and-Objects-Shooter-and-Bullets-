7a90528f6eb91c9a3548b2d45a56ff8bcb2a48e6
Your Name <hibahmohammad1228@gmail.com>
2026-04-24 16:29:32 -0500

    still need to finish...

--- hibahm_classescode.py
+++ hibahm_classescode.py
@@ -20,7 +20,7 @@ def playing_area():
     pen.end_fill()
 
 class Player(Turtle):
-    def __init__(self, x, y, color, screen, right_key, left_key):
+    def __init__(self, x, y, color, screen, right_key, left_key, fire_key):
         super().__init__()
         self.ht()
         self.speed(0)
@@ -38,7 +38,7 @@ class Player(Turtle):
 
         screen.onkeypress(self.turn_left, left_key)
         screen.onkeypress(self.turn_right, right_key)
-        screen.onkeypress(self.fire, "space")
+        screen.onkeypress(self.fire, fire_key)
 
     def fire(self):
         if self.alive:
@@ -65,39 +65,30 @@ class Player(Turtle):
 
 
 class Bullet(Turtle):
-    def __init__(self, x, y, heading):
+    def __init__(self, player):
         super().__init__()
+        self.ht()
+        self.speed(0)
         self.penup()
         self.shape("circle")
-        self.color("yellow")
-        self.goto(x, y)
-        self.setheading(heading)
-        self.alive = True
+        self.color(player.color)
+        self.goto(player.xcor(), player.ycor())
+        self.setheading(player.heading())
+        self.forward(10)
+        self.player = player
+        self.st()
 
     def move(self):
-        if self.alive:
+        if self.player.fire_key:
             self.forward(10)
-
             if self.xcor() > 235 or self.xcor() < -235 or self.ycor() > 235 or self.ycor() < -235:
                 self.die()
 
     def die(self):
-        self.alive = False
+
         self.hideturtle()
 
 
-def check_hit(shooter, target, bullet):
-    bx = bullet.xcor()
-    by = bullet.ycor()
-    tx = target.xcor()
-    ty = target.ycor()
-
-    if -15 < bx - tx < 15 and -15 < by - ty < 15:
-        target.health -= 1
-        bullet.die()
-
-        if target.health <= 0:
-            target.kill()
 
 
 screen = Screen()
@@ -107,8 +98,8 @@ screen.listen()
 
 playing_area()
 
-p1 = Player(-100, 0, "red", screen, "d", "a")
-p2 = Player(100, 0, "blue", screen, "Right", "Left")
+p1 = Player(-100, 0, "purple", screen, "d", "a","w")
+p2 = Player(100, 0, "blue", screen, "Right", "Left","Up")
 
 while p1.alive and p2.alive:
     p1.move()
@@ -117,17 +108,6 @@ while p1.alive and p2.alive:
     new_bullets = []
     for b in p1.bullets:
         b.move()
-        check_hit(p1, p2, b)
-        if b.alive:
-            new_bullets.append(b)
-    p1.bullets = new_bullets
-
-    new_bullets = []
-    for b in p2.bullets:
-        b.move()
-        check_hit(p2, p1, b)
-        if b.alive:
-            new_bullets.append(b)
-    p2.bullets = new_bullets
+        
 
 screen.exitonclick()
